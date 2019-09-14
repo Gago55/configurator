3dc5b799033147a309c80abfd3cd7530f59e094b
gago55 <xgagik8@gmail.com>
2019-09-14 19:30:53 +0400

    Added Three.js and ThreeScene Component

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+// let threeObj = new ThreeJS()
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// animate();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
