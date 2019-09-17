1d83969b582809ec91aad02b9a5e10267d969495
gago55 <xgagik8@gmail.com>
2019-09-17 16:29:47 +0400

    Updates

---
+++ precache-manifest.a38ecdf1c2b9af6739ae87330f2aa352.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564b46abe6a8d7c6b940d7578b822506",
+    "url": "/configurator/index.html"
+  },
+  {
+    "revision": "b7e6469a16244346e330",
+    "url": "/configurator/static/css/main.c9124a1d.chunk.css"
+  },
+  {
+    "revision": "53fc72bbc95c5b63398f",
+    "url": "/configurator/static/js/2.957b35e6.chunk.js"
+  },
+  {
+    "revision": "b7e6469a16244346e330",
+    "url": "/configurator/static/js/main.bbba98b0.chunk.js"
+  },
+  {
+    "revision": "5dcb7b05f9fed1cb306e",
+    "url": "/configurator/static/js/runtime~main.40caa27d.js"
+  }
+]);
