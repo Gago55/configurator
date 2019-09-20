75f873ea884d3208f4b0f40b82cdbf72bdb684af
gago55 <xgagik8@gmail.com>
2019-09-20 17:40:52 +0400

    Changes For Redux Exstention

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import {combineReducers, createStore} from 'redux'
+import {combineReducers, createStore, compose, applyMiddleware} from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import threeReducer from './threeReducer';
 import configReducer from './configReducer';
 
@@ -7,6 +8,11 @@ let reducers =  combineReducers({
     config : configReducer
 })
 
-let store = createStore(reducers)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers
+    ,composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+  ))
+// let store = createStore(reducers)
 
 export default store
